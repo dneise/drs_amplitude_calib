da6f0b847baaa0be9e902096a9a9a9febb71acbb
Dominik Neise <neised@phys.ethz.ch>
2016-08-23 13:03:10 +0200

    bugfixes

--- make_all_drs_step2_h5.py
+++ make_all_drs_step2_h5.py
@@ -9,7 +9,7 @@ import numpy as np
 from tqdm import tqdm
 
 db = fact.credentials.create_factdb_engine()
-all_drs_step2 = pd.read_sql("select * from RunInfo where fDrsStep=2 and fNight>20150528 and fROI==300", db)
+all_drs_step2 = pd.read_sql("select * from RunInfo where fDrsStep=2 and fNight>20160528 and fROI=300", db)
 all_drs_step2["drs_temp_mean"] = np.nan
 all_drs_step2["drs_temp_rms"] = np.nan
 
@@ -20,11 +20,12 @@ for n, g in tqdm(all_drs_step2.groupby("fNight")):
     n = str(n)
     y,m,d = n[0:4], n[4:6], n[6:8]
     path = "/fact/aux/{y}/{m}/{d}/{n}.FAD_CONTROL_TEMPERATURE.fits".format(y=y, m=m, d=d, n=n)
+
     try:
         t = Table.read(path)
         t["datetime"] = pd.to_datetime(t["Time"], unit="d")
 
-        for row_id in tqdm(range(len(g))):
+        for row_id in range(len(g)):
             row = g.iloc[row_id]
             r = row.fRunID
             start = row.fRunStart
@@ -34,8 +35,9 @@ for n, g in tqdm(all_drs_step2.groupby("fNight")):
             if len(payload):
                 row["drs_temp"] = payload.mean()
                 row["drs_temp_rms"] = payload.std()
+    except FileNotFoundError:
+        pass
 
-    
 all_drs_step2["duration"] = all_drs_step2.fRunStop - all_drs_step2.fRunStart
 all_drs_step2["Time"] = all_drs_step2.fRunStart + all_drs_step2.duration/2
 all_drs_step2.set_index("Time", inplace=True)
